4753c867c747d41c2091774fb373f5ab03c87419
Connor Talford <ctalford@softwareinstitute.com>
2022-03-30 12:55:26 +0100

    second commit

---
+++ build/CMakeFiles/say-hello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/say-hello.dir/src/hello.cpp.obj"
+  "CMakeFiles/say-hello.dir/src/hello.cpp.obj.d"
+  "libsay-hello.dll"
+  "libsay-hello.dll.a"
+  "libsay-hello.dll.manifest"
+  "libsay-hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/say-hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
